8c22f5ebe8901aed0e90b6622cae50fd1a2b1b81
lzerone <lzerone@163.com>
2022-07-16 18:47:24 +0800

    speed

---
+++ src/shared/ShaAllocator.h
@@ -0,0 +1,19 @@
+#ifndef _SHAALLOCATOR_H_
+#define _SHAALLOCATOR_H_
+
+namespace Ser {
+    class ShaAllocator
+    {
+    private:
+        /* data */
+    public:
+        ShaAllocator(/* args */) {
+
+        }
+        ~ShaAllocator() {
+            
+        }
+    };
+}
+
+#endif // _SHAALLOCATOR_H_
--- test/test_LockFreeRingBuffer.cpp
+++ test/test_LockFreeRingBuffer.cpp
@@ -14,6 +14,7 @@ using namespace Ser::Detail;
 using namespace Ser;
 using namespace std;
 
+// 为630时则刚好为
 #define MALLOC_DATA_SIZE 630
 typedef struct MallocDataS
 {
@@ -74,10 +75,10 @@ TEST(LockFreeRingBuffer, mallocAllocator)
         // cout << "ret:" << ret << " outLen:" << outLen
         //      << " avaliable:" << avaliable << endl;
 
-        for (int tmp = 0; tmp < rdSize; tmp++)
-        {
-            reader[tmp].Dump();
-        }
+        // for (int tmp = 0; tmp < rdSize; tmp++)
+        // {
+        //     reader[tmp].Dump();
+        // }
     }
     end = std::chrono::steady_clock::now();
     elapsed_seconds = std::chrono::duration_cast<std::chrono::microseconds>(
@@ -87,10 +88,16 @@ TEST(LockFreeRingBuffer, mallocAllocator)
     std::cout << "elapsed time: " << elapsed_seconds << "us\n";
 }
 
-TEST(LockFreeRingBuffer, sharedMem)
+// 该测试用例是共享内存的
+TEST(LockFreeRingBuffer, sharedMemSpSc)
 {
     cout << "size:" << sizeof(MallocDataT) << "byte" << endl;
-    uint32_t size = 512;
+
+    uint32_t size = 300000;
+    int loopTimes = 1000000;
+    size_t all = sizeof(MallocDataT) * loopTimes;
+    cout << "size:" << (all >> 20) << "mb" << endl;
+
     void *buffer = mmap(NULL,
                         sizeof(MallocDataT) * size,
                         PROT_READ | PROT_WRITE,
@@ -113,7 +120,6 @@ TEST(LockFreeRingBuffer, sharedMem)
     uint32_t free = 0;
     uint32_t ret = 0;
     auto start = std::chrono::steady_clock::now();
-    int loopTimes = 1000000;
     for (int idx = 0; idx < loopTimes; idx++)
     {
         for (int tt = 0; tt < wrSize; tt++)
@@ -156,4 +162,11 @@ TEST(LockFreeRingBuffer, sharedMem)
             .count();
 
     std::cout << "elapsed time: " << elapsed_seconds << "us\n";
+
+    munmap(buffer, sizeof(MallocDataT) * size);
+}
+
+TEST(LockFreeRingBuffer, sharedMemIngSpSc)
+{
+
 }
