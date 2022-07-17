07ce4bc711e675ba7e5cd6535b6301deec2f6633
lzerone <lzerone@163.com>
2022-07-18 01:01:09 +0800

    提交ticket lock以及wrlock ticket lock的try lock未实现

--- src/lock/ShaWRLock.h
+++ src/lock/ShaWRLock.h
@@ -1,4 +1,90 @@
 #ifndef _SHAWRLOCK_H_
 #define _SHAWRLOCK_H_
 
+#include <atomic>
+#include <cstdint>
+
+#include "NoCopyable.h"
+
+namespace Ser {
+    // 因为在共享内存里面锁都是无效的 所以自己实现
+    class ShaWRlock {
+        public:
+            void RdLock() {
+                int32_t tmp = 0;
+                bool success = false;
+
+                while (!success)
+                {
+                    tmp = lk.load(std::memory_order_relaxed);
+                    if (tmp < 0) {
+                        continue;
+                    }
+
+                    success = lk.compare_exchange_weak(tmp, tmp + 1, std::memory_order_acquire, std::memory_order_relaxed);
+                }
+                
+            }
+
+            void RdUnLock() {
+                lk.fetch_sub(1, std::memory_order_release);
+            }
+
+            bool TryRdLock() {
+                int32_t tmp = 0;
+                bool success = false;
+
+                while (!success)
+                {
+                    tmp = lk.load(std::memory_order_relaxed);
+                    if (tmp < 0)
+                    {
+                        return false;
+                    }
+
+                    success =
+                        lk.compare_exchange_weak(tmp,
+                                                 tmp + 1,
+                                                 std::memory_order_acquire,
+                                                 std::memory_order_relaxed);
+                }
+
+                return true;
+            }
+
+            void WrLock() {
+                int32_t tmp = 0;
+                bool success = false;
+
+                while (!success)
+                {
+                    tmp = lk.load(std::memory_order_relaxed);
+                    if (tmp != 0)
+                    {
+                        continue;
+                    }
+
+                    success =
+                        lk.compare_exchange_weak(tmp,
+                                                 -1,
+                                                 std::memory_order_acquire,
+                                                 std::memory_order_relaxed);
+                }
+            }
+
+            void WrUnLock()
+            {
+                lk.store(0, std::memory_order_release);
+            }
+
+            bool TryWrLock() {
+                int32_t tmp = lk.load(std::memory_order_relaxed);
+                return tmp != 0 || lk.compare_exchange_weak(tmp, -1, std::memory_order_acquire, std::memory_order_release);
+            }
+
+        private:
+            std::atomic<int32_t> lk{0};
+    };
+}
+
 #endif  // _SHAWRLOCK_H_
--- src/lock/TicketLock.h
+++ src/lock/TicketLock.h
@@ -1,4 +1,35 @@
 #ifndef _TICKETLOCK_H_
 #define _TICKETLOCK_H_
 
+#include <atomic>
+
+#include "NoCopyable.h"
+
+namespace Ser {
+    // 比起自旋锁特点是谁先到谁获得
+    class TicketLock : public NoCopyable {
+        public:
+            void lock() {
+                uint16_t seq =
+                    next.fetch_add(1, std::memory_order_relaxed);
+                while (cur.load(std::memory_order_acquire) != seq)
+                {
+                    /* code */
+                    // 不等于就空转
+                }
+            }
+
+            void unlock() {
+                uint16_t tmp = cur.load(std::memory_order_relaxed);
+                cur.store(tmp + 1, std::memory_order_release);
+            }
+
+            private:
+              /* data */
+              std::atomic<uint16_t> cur{0};
+              std::atomic<uint16_t> next{0};
+    };
+}
+
+
 #endif  // _TICKETLOCK_H_
