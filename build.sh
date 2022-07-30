ae8f9404c8c4ff29c20a0a6cafdd300a62c12a53
lzerone <lzerone@163.com>
2022-07-30 20:33:45 +0800

    提交新编译依赖下载

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -eux
 rm -rf build/
 mkdir build
 cd build
-cmake -DCMAKE_VERBOSE_MAKEFILE=on ..
+cmake -DCMAKE_VERBOSE_MAKEFILE=on -DDOWNLOAD_THIRDPARTY=off ..
 make
--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -1,13 +1,13 @@
-message("download thirdparty")
+message("download thirdparty ${CMAKE_MAKE_PROGRAM}")
 
-# 下载gtest github经常断 所以使用gitee的开源库了
-include(FetchContent)
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://gitee.com/zhang1021/googletest.git
-  GIT_TAG        release-1.10.0 # release-1.10.0
-)
-FetchContent_MakeAvailable(googletest)
+# # 下载gtest github经常断 所以使用gitee的开源库了
+# include(FetchContent)
+# FetchContent_Declare(
+#   googletest
+#   GIT_REPOSITORY https://gitee.com/zhang1021/googletest.git
+#   GIT_TAG        release-1.10.0 # release-1.10.0
+# )
+# FetchContent_MakeAvailable(googletest)
 
 # include(ExternalProject)
 # ExternalProject_Add(googletest
@@ -20,3 +20,20 @@ FetchContent_MakeAvailable(googletest)
 #     INSTALL_COMMAND ""
 #     TEST_COMMAND ""
 # )
+
+# 类似https://blog.csdn.net/zym326975/article/details/106118063
+include(ExternalProject)
+ExternalProject_Add(gtest
+  INSTALL_DIR ${CMAKE_SOURCE_DIR}/third
+  GIT_REPOSITORY https://gitee.com/zhang1021/googletest.git
+  GIT_TAG        release-1.10.0 # release-1.10.0
+  PREFIX ${CMAKE_SOURCE_DIR}/source/third
+  CONFIGURE_COMMAND ""
+  CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+              -DCMAKE_BUILD_TYPE=Release
+              -DCMAKE_MAKE_PROGRAM={CMAKE_MAKE_PROGRAM}
+              -DCMAKE_C_COMPILE={CMAKE_C_COMPILE}
+              -DCMAKE_CXX_COMPILE={CMAKE_CXX_COMPILE}
+              -DCMAKE_VERBOSE_MAKEFILE={CMAKE_VERBOSE_MAKEFILE}
+  # INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+  )
